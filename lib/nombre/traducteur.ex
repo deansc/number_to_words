69fefb5d4ab8db32f7491bd47c4ae8df21ba1a0c
Deans Charbal <deansfa@gmail.com>
2015-09-03 18:07:39 -0400

    Refactor.

--- lib/nombre/traducteur.ex
+++ lib/nombre/traducteur.ex
@@ -1,17 +1,24 @@
 defmodule Nombre.Traducteur do 
 
+  def number_to_words(0), do: Nombre.Dictionnaire.english(0)
   def number_to_words(n) do
     n 
     |> decompose
     |> translate(0)
+    |> String.rstrip
   end
 
   def decompose(n) do 
-    n
-    |> Integer.to_string
-    |> String.split("", trim: true)
+    n 
+    |> do_decompose([])
     |> Enum.reverse
-    |> Enum.map &(String.to_integer(&1))
+  end
+
+  # can be replaced by Integer.digits(n) when Elixir v.1.1 is released
+  defp do_decompose(0, []), do: [0]
+  defp do_decompose(0, acc), do: acc
+  defp do_decompose(n, acc) do
+    do_decompose(div(n, 10), [rem(n, 10) | acc])
   end
 
   def compose(arr) do 
@@ -19,54 +26,69 @@ defmodule Nombre.Traducteur do
     |> Enum.reverse
     |> Enum.reduce(0, &(&2 * 10 + &1))
   end
-  
-  def translate(ary, 0) when is_list(ary) and length(ary) in (1..3) do 
-    translate_3_digits(ary)
+
+  def translate([], _), do: ""
+  def translate(ary, n) when  is_list(ary) 
+                        and   length(ary) in (1..3) do 
+    last = List.last(ary)
+
+    case last do
+      0 -> List.delete_at(ary, -1) |> translate(n)
+      _ -> translate_3_digits(ary, n)
+    end
   end
 
-  def translate(ary, n) when is_list(ary) and length(ary) in (1..3) do 
-    [
-      translate_3_digits(ary),
-      translate_magnitude(n)
-    ] 
-    |> Enum.join(" ")
-  end
-  
-  def translate([a, b, c | tail], n) do
+  def translate([a, b, c | tail], n) do 
     [
       translate(tail, n + 1), 
-      translate_3_digits([a, b, c]),
-      translate_magnitude(n)
-    ] 
-    |> Enum.join(" ")
+      translate([a, b, c], n)
+    ] |> Enum.join(" ")
   end
 
-  def translate_3_digits([a]), do: Nombre.Dictionnaire.english(a)
-  def translate_3_digits([a, b]) when b in (0..1), do: Nombre.Dictionnaire.english(a + b * 10)
-
-  def translate_3_digits([a, 0, 0]), do: Nombre.Dictionnaire.english(a)
-
-  def translate_3_digits([0, b]), do: Nombre.Dictionnaire.english(b * 10)
-  def translate_3_digits([a, b]) do
-    [translate_3_digits([0, b]), translate_3_digits([a])] 
-    |> Enum.join(" ")
+  def translate_3_digits(list, magnitude) do 
+    case magnitude do 
+      0 -> translate_3_digits(list)
+      _ -> [translate_3_digits(list), translate_magnitude(magnitude)] |> Enum.join(" ")
+    end
   end
 
-  def translate_3_digits([0, 0, c]) do 
-    [translate_3_digits([c]), Nombre.Dictionnaire.english(100)]
-    |> Enum.join(" ")
+  def translate_3_digits([a]) do 
+    case a do 
+      0 -> ""
+      _ -> Nombre.Dictionnaire.english(a)
+    end
+  end
+
+  def translate_3_digits([a, b]) do 
+    case [a, b] do
+      [0, 0] -> "" 
+      [_, 0] -> Nombre.Dictionnaire.english(a + b * 10)
+      [_, 1] -> Nombre.Dictionnaire.english(a + b * 10)
+      [_, _] -> [Nombre.Dictionnaire.english(b * 10), translate_3_digits([a])] |> Enum.join(" ")
+    end
   end
 
   def translate_3_digits([a, b, c]) do 
-    [translate_3_digits([0, 0, c]), translate_3_digits([a, b])]
-    |> Enum.join(" ")
+    case c do 
+      0 -> translate_3_digits([a, b])
+      _ -> 
+        [
+          translate_3_digits([c]), 
+          Nombre.Dictionnaire.english(100), 
+          translate_3_digits([a, b])
+        ] 
+        |> Enum.join(" ")
+    end
   end
 
-  def translate_magnitude(0), do: ""
-  def translate_magnitude(4), do: ""
-  def translate_magnitude(n) when n < 4 do
-    :math.pow(1000, n) 
-    |> round 
-    |> Nombre.Dictionnaire.english
+  def translate_magnitude(n) when n <= 4 do
+    case n do 
+      0 -> ""
+      4 -> ""
+      _ ->
+        :math.pow(1000, n) 
+        |> round 
+        |> Nombre.Dictionnaire.english
+    end    
   end
 end
--- test/nombre/traducteur_test.exs
+++ test/nombre/traducteur_test.exs
@@ -2,9 +2,31 @@ defmodule TraducteurTest do
   use ExUnit.Case
 
   test ":number_to_words should return the equivalent English word of a integer" do
-    assert Nombre.Traducteur.number_to_words(1) === "one"
+    assert Nombre.Traducteur.number_to_words(1)  == "one"
+    assert Nombre.Traducteur.number_to_words(5)  == "five"
+    assert Nombre.Traducteur.number_to_words(10) == "ten"
+    assert Nombre.Traducteur.number_to_words(20) == "twenty"
+    assert Nombre.Traducteur.number_to_words(22) == "twenty two"
+    assert Nombre.Traducteur.number_to_words(34) == "thirty four"
+    assert Nombre.Traducteur.number_to_words(99) == "ninety nine"
 
-    assert Nombre.Traducteur.number_to_words(3543543) === "three million five hundred forty three thousand five hundred forty three"
+    assert Nombre.Traducteur.number_to_words(100) == "one hundred"
+    assert Nombre.Traducteur.number_to_words(101) == "one hundred one"
+    assert Nombre.Traducteur.number_to_words(110) == "one hundred ten"
+    assert Nombre.Traducteur.number_to_words(120) == "one hundred twenty"
+    assert Nombre.Traducteur.number_to_words(130) == "one hundred thirty"
+    assert Nombre.Traducteur.number_to_words(135) == "one hundred thirty five"
+    assert Nombre.Traducteur.number_to_words(434) == "four hundred thirty four"
+
+    assert Nombre.Traducteur.number_to_words(900) == "nine hundred"
+    assert Nombre.Traducteur.number_to_words(999) == "nine hundred ninety nine"
+
+    assert Nombre.Traducteur.number_to_words(1000000) == "one million"
+    assert Nombre.Traducteur.number_to_words(2000000) == "two million"
+    assert Nombre.Traducteur.number_to_words(3543543) == "three million five hundred forty three thousand five hundred forty three"
+
+    assert Nombre.Traducteur.number_to_words(765434) == "seven hundred sixty five thousand four hundred thirty four"
+    assert Nombre.Traducteur.number_to_words(1765434) == "one million seven hundred sixty five thousand four hundred thirty four"
   end
 
   test ":compose should return an integer from an array of its inverted digits" do
@@ -16,43 +38,48 @@ defmodule TraducteurTest do
     assert Nombre.Traducteur.decompose(948379432)  === [2, 3, 4, 9, 7, 3, 8, 4, 9]
   end
 
-  test ":translate should translate an array of digits and magnitude to a word" do 
-    assert Nombre.Traducteur.translate([4, 3, 4], 0) == "four hundred thirty four"
-    assert Nombre.Traducteur.translate([4, 3, 4], 1) == "four hundred thirty four thousand"
+  # test ":translate_3_digits/2 should translate an array of three digits and a magnitude to a word" do 
 
-    assert Nombre.Traducteur.translate([4, 3, 4, 5, 6, 7], 0) == "seven hundred sixty five thousand four hundred thirty four "
-    assert Nombre.Traducteur.translate([4, 3, 4, 5, 6, 7, 1], 0) == "one million seven hundred sixty five thousand four hundred thirty four"
-  end
+  #   assert Nombre.Traducteur.translate_3_digits([2, 2], 0) == "twenty two"
+  #   assert Nombre.Traducteur.translate_3_digits([4, 3], 1) == "thirty four thousand"
+  #   assert Nombre.Traducteur.translate_3_digits([9, 9], 2) == "ninety nine million"
 
-  test ":translate_3_digits should translate an array of three digits to a word" do 
-    assert Nombre.Traducteur.translate_3_digits([0])    == "zero"
-    assert Nombre.Traducteur.translate_3_digits([1])    == "one"
-    assert Nombre.Traducteur.translate_3_digits([5])    == "five"
-    assert Nombre.Traducteur.translate_3_digits([1, 0]) == "one"
-    assert Nombre.Traducteur.translate_3_digits([5, 0]) == "five"
-    assert Nombre.Traducteur.translate_3_digits([0, 1]) == "ten"
-    assert Nombre.Traducteur.translate_3_digits([0, 2]) == "twenty"
-    assert Nombre.Traducteur.translate_3_digits([2, 2]) == "twenty two"
-    assert Nombre.Traducteur.translate_3_digits([4, 3]) == "thirty four"
-    assert Nombre.Traducteur.translate_3_digits([9, 9]) == "ninety nine"
+  #   assert Nombre.Traducteur.translate_3_digits([1, 0, 0], 1) == "one thousand"
 
-    assert Nombre.Traducteur.translate_3_digits([0, 0, 1]) == "one hundred"
-    assert Nombre.Traducteur.translate_3_digits([1, 0, 1]) == "one hundred one"
-    assert Nombre.Traducteur.translate_3_digits([2, 0, 1]) == "one hundred two"
-    assert Nombre.Traducteur.translate_3_digits([0, 2, 1]) == "one hundred twenty"
-    assert Nombre.Traducteur.translate_3_digits([3, 2, 1]) == "one hundred twenty three"
-    assert Nombre.Traducteur.translate_3_digits([0, 3, 1]) == "one hundred thirty"
-    assert Nombre.Traducteur.translate_3_digits([4, 3, 4]) == "four hundred thirty four"
-    assert Nombre.Traducteur.translate_3_digits([5, 3, 1]) == "one hundred thirty five"
-  end
+  #   assert Nombre.Traducteur.translate_3_digits([0, 0, 1], 0) == "one hundred"
+  #   assert Nombre.Traducteur.translate_3_digits([1, 0, 1], 1) == "one hundred one thousand"
+  #   assert Nombre.Traducteur.translate_3_digits([2, 0, 1], 2) == "one hundred two million"
+  #   assert Nombre.Traducteur.translate_3_digits([0, 2, 1], 3) == "one hundred twenty billion"
+  # end
+
+  # test ":translate should translate an array of digits and magnitude to a word" do 
+  #   assert Nombre.Traducteur.translate([4, 3, 4], 0) == "four hundred thirty four"
+  #   assert Nombre.Traducteur.translate([4, 3, 4], 1) == "four hundred thirty four thousand"
+
+  #   assert Nombre.Traducteur.translate([4, 3, 4, 5, 6, 7], 0) == "seven hundred sixty five thousand four hundred thirty four "
+  #   assert Nombre.Traducteur.translate([4, 3, 4, 5, 6, 7, 1], 0) == "one million seven hundred sixty five thousand four hundred thirty four"
+  # end
+
+  # test ":translate_3_digits should translate an array of three digits to a word" do 
 
 
-  test ":translate_magnitude should return thousand, millions, " do 
-    assert Nombre.Traducteur.translate_magnitude(0) == ""
-    assert Nombre.Traducteur.translate_magnitude(1) == "thousand"
-    assert Nombre.Traducteur.translate_magnitude(2) == "million"
-    assert Nombre.Traducteur.translate_magnitude(3) == "billion"
-    assert Nombre.Traducteur.translate_magnitude(4) == ""
-  end
+  #   assert Nombre.Traducteur.translate_3_digits([0, 0, 1]) == "one hundred"
+  #   assert Nombre.Traducteur.translate_3_digits([1, 0, 1]) == "one hundred one"
+  #   assert Nombre.Traducteur.translate_3_digits([2, 0, 1]) == "one hundred two"
+  #   assert Nombre.Traducteur.translate_3_digits([0, 2, 1]) == "one hundred twenty"
+  #   assert Nombre.Traducteur.translate_3_digits([3, 2, 1]) == "one hundred twenty three"
+  #   assert Nombre.Traducteur.translate_3_digits([0, 3, 1]) == "one hundred thirty"
+  #   assert Nombre.Traducteur.translate_3_digits([4, 3, 4]) == "four hundred thirty four"
+  #   assert Nombre.Traducteur.translate_3_digits([5, 3, 1]) == "one hundred thirty five"
+  # end
+
+
+  # test ":translate_magnitude should return thousand, millions, " do 
+  #   assert Nombre.Traducteur.translate_magnitude(0) == ""
+  #   assert Nombre.Traducteur.translate_magnitude(1) == "thousand"
+  #   assert Nombre.Traducteur.translate_magnitude(2) == "million"
+  #   assert Nombre.Traducteur.translate_magnitude(3) == "billion"
+  #   assert Nombre.Traducteur.translate_magnitude(4) == ""
+  # end
 
 end
